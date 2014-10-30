0a5e519e6622575182e36a21698911ec4c810de2
CristiBB <boboc.cristi@gmail.com>
2014-10-30 13:33:16 +0200

    Routes and views added

--- routes/index.js
+++ routes/index.js
@@ -22,6 +22,46 @@ router.get('/map_senate', function (req, res) {
     });
 });
 
+router.get('/map_chamber_of_deputies', function (req, res) {
+    res.render('map_chamber_of_deputies', {
+        title: 'Chamber of deputies map'
+    });
+});
+
+/* GET About page. */
+router.get('/about', function (req, res) {
+    res.render('about', {
+        title: 'About the project'
+    });
+});
+
+/* GET Integration page. */
+router.get('/integration', function (req, res) {
+    res.render('integration', {
+        title: 'Integration'
+    });
+});
+
+/* GET Collaborators page. */
+router.get('/collaborators', function (req, res) {
+    res.render('collaborators', {
+        title: 'Collaborators and Data Sources'
+    });
+});
+
+/* GET Feedback page. */
+router.get('/feedback', function (req, res) {
+    res.render('feedback', {
+        title: 'Feedback'
+    });
+});
+
+/* GET Statistics page. */
+router.get('/statistics', function (req, res) {
+    res.render('statistics', {
+        title: 'Statistics'
+    });
+});
 
 /* Page not found - if the path is not defined in this file */
 router.get('*', function (req, res) {
