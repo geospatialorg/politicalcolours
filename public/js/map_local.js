f04126dd346673918a3b46a471785fb62b8d69b9
Bogdan Grama <bogdan.grama@gmail.com>
2014-10-30 14:03:42 +0200

    Derpy hooves

--- public/js/map_local.js
+++ public/js/map_local.js
@@ -1,18 +1,28 @@
-var northB = [57, 37];
-var southB = [33.0, 13.0];
+/*jslint browser: true*/
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50 */
+/*global $*/
+/*global L*/
+
+var map;
+var northB = [51.26, 34.71];
+var southB = [40.61, 15.26];
 
 /* Basemap Layers */
 var mapquestOSM = L.tileLayer("http://{s}.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png", {
-    maxZoom: 17,
+    maxZoom: 16,
     subdomains: ["otile1", "otile2", "otile3", "otile4"],
     bounds: new L.LatLngBounds(southB, northB),
+    noWrap: true,
     attribution: 'Tiles courtesy of <a href="http://www.mapquest.com/" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png">. Map data (c) <a href="http://www.openstreetmap.org/" target="_blank">OpenStreetMap</a> contributors, CC-BY-SA.'
 });
 
-var map = L.map("map", {
+map = L.map("map", {
     zoom: 7,
-    center: [44.07797245672571, 27.197773080210172],
+    center: [46.07, 25.19],
     layers: [mapquestOSM],
     zoomControl: true,
-    attributionControl: false
+    attributionControl: true,
+    maxZoom: 16,
+    minZoom: 7,
+    maxBounds: new L.LatLngBounds(southB, northB),
 });
