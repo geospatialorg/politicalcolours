e6c24d1117f2e53a7be29461da5e956ab7046e70
CristiBB <boboc.cristi@gmail.com>
2014-11-01 11:05:26 +0200

    Maps added

--- routes/map.js
+++ routes/map.js
@@ -10,6 +10,34 @@ router.get('/local', function (req, res) {
     });
 });
 
+router.get('/local/county_councils_presidents_2012', function (req, res) {
+    res.render('maps/county_councils_presidents_2012', {
+		meta: config.meta,
+        title: 'Presidents of the County Councils 2012'
+    });
+});
+
+router.get('/local/mayors_2012', function (req, res) {
+    res.render('maps/mayors_2012', {
+		meta: config.meta,
+        title: 'Mayors 2012'
+    });
+});
+
+router.get('/local/mayors_2014_after_migration', function (req, res) {
+    res.render('maps/mayors_2014_after_migration', {
+		meta: config.meta,
+        title: 'Mayors 2014 after migration'
+    });
+});
+
+router.get('/local/senate_actualized', function (req, res) {
+    res.render('maps/senate_actualized', {
+		meta: config.meta,
+        title: 'Senate map actualized'
+    });
+});
+
 router.get('/senate', function (req, res) {
     res.render('map_senate', {
 		meta: config.meta,
