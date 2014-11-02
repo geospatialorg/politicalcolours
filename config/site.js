cdc254373ed26c65eb110122b656376bbe600644
CristiBB <boboc.cristi@gmail.com>
2014-11-02 13:09:12 +0200

    Fix

--- config/site.js
+++ config/site.js
@@ -2,7 +2,7 @@ module.exports = {
 	"meta":  {
 		"path" : "",
 		"site_title" : "politicalcolours.ro",
-		"site_url" : "http://politicalcolours.ro/reloaded/",
+		"site_url" : "localhost:8888/reloaded/",
 		"site_description": "Lorem ipsum"
 	}
 	
--- routes/map.js
+++ routes/map.js
@@ -2,10 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 function checkIfEmbeded(req, res, next){
-	
-	//if(res.query.embed){
-		//res.locals.embed = true;
-	//}
+	res.locals.embed = false;
+	if(typeof req.query.embed != undefined && req.query.embed){
+		res.locals.embed = true;
+	}
 	next();
 }
 
@@ -15,63 +15,63 @@ router.get('/local/county_councils_presidents_2012', checkIfEmbeded,  function (
     });
 });
 
-router.get('/local/mayors_2012', function (req, res) {
+router.get('/local/mayors_2012', checkIfEmbeded, function (req, res) {
     res.render('maps/mayors_2012', {
         title: 'Mayors 2012'
     });
 });
 
-router.get('/local/mayors_2014_after_migration', function (req, res) {
+router.get('/local/mayors_2014_after_migration', checkIfEmbeded, function (req, res) {
     res.render('maps/mayors_2014_after_migration', {
         title: 'Mayors 2014 OUG 55/2014'
     });
 });
 
-router.get('/local/dna_activity_mayors', function (req, res) {
+router.get('/local/dna_activity_mayors', checkIfEmbeded, function (req, res) {
     res.render('maps/dna_activity_mayors', {
         title: 'DNA Activity Mayors'
     });
 });
 
-router.get('/senate/senate_2008-2012', function (req, res) {
+router.get('/senate/senate_2008-2012', checkIfEmbeded, function (req, res) {
     res.render('maps/senate_2008-2012', {
         title: 'Senate 2008-2012'
     });
 });
 
-router.get('/senate/senate_2012-2016', function (req, res) {
+router.get('/senate/senate_2012-2016', checkIfEmbeded, function (req, res) {
     res.render('maps/senate_2012-2016', {
         title: 'Senate 2012-2016'
     });
 });
 
-router.get('/chamber_of_deputies/chamber_of_deputies_2008-2012', function (req, res) {
+router.get('/chamber_of_deputies/chamber_of_deputies_2008-2012', checkIfEmbeded, function (req, res) {
     res.render('maps/chamber_of_deputies_2008-2012', {
         title: 'Chamber of deputies 2008-2012'
     });
 });
 
-router.get('/chamber_of_deputies/chamber_of_deputies_2012-2016', function (req, res) {
+router.get('/chamber_of_deputies/chamber_of_deputies_2012-2016', checkIfEmbeded, function (req, res) {
     res.render('maps/chamber_of_deputies_2012-2016', {
         title: 'Chamber of deputies 2012-2016'
     });
 });
 
 /* GET Statistics page. */
-router.get('/statistics/population_census_2011', function (req, res) {
+router.get('/statistics/population_census_2011', checkIfEmbeded, function (req, res) {
     res.render('maps/population_census_2011', {
         title: 'Population Census 2011'
     });
 });
 
 
-router.get('/statistics/campaign_tracker_2014', function (req, res) {
+router.get('/statistics/campaign_tracker_2014', checkIfEmbeded, function (req, res) {
     res.render('maps/campaign_tracker_2014', {
         title: 'Campaign Tracker 2014'
     });
 });
 
-router.get('/statistics/motorways', function (req, res) {
+router.get('/statistics/motorways', checkIfEmbeded, function (req, res) {
     res.render('maps/motorways', {
         title: 'motorways'
     });
